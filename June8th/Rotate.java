f08656f11cbbca1d23e0850f5de7408c723401f2
makshay17 <134974810+makshay17@users.noreply.github.com>
2023-06-09 12:12:18 -0500

    123

---
+++ June8th/Rotate.java
@@ -0,0 +1,33 @@
+package June8th;
+
+public class Rotate{
+
+    boolean flag=true;
+    public void test(String round,String xyz) {
+        for (int i = 0; i <= round.length(); i++) {
+            if (round.equals(xyz)) {
+                flag = true;
+                break;
+            }
+            else {
+                char c = round.charAt(0);
+                round = round.substring(1);
+                if ((round= round + c).equals(xyz)) {
+                    flag = true;
+                    break;
+                } else {
+                    flag = false;
+                }
+            }
+
+
+
+        }
+        System.out.println(flag);
+    }
+    public static void main(String[] args){
+        Rotate m=new Rotate();
+        m.test("arya","arya");
+
+    }
+}
